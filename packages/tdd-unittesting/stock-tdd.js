6c8c3f8d99cfc9290a38dfb9fffcd58a0be57b4a
Darryl <darryljamescruz@gmail.com>
2025-01-28 23:24:55 -0800

    2.3 - 2.5 implemented w/ TDD

--- packages/tdd-unittesting/stock-tdd.js
+++ packages/tdd-unittesting/stock-tdd.js
@@ -1,9 +1,33 @@
 class StockPortfolio {
     constructor() {
-        this.stocks = new Map();    //Map to store stock and their shares
+        this.stocks = new Map();    // Map to store stock and their shares (STOCK, SHARES)
     }
+
     isEmpty() {
         return this.stocks.size === 0;  
+    }
+
+    addStock(symbol, shares) {
+        if (!Number.isInteger(shares) || shares <= 0) throw new Error('Shares must be a positive integer'); // validate # of shares
+
+        this.stocks.set(symbol, (this.stocks.get(symbol) || 0) + shares)
+    }
+    sellStock(symbol, shares) {
+        if (!this.stocks.has(symbol)) throw new Error('Stock not found in portfolio');  // validate stock symbol which should be in portfolio
+        if (!Number.isInteger(shares) || shares <= 0) throw new Error('Shares must be a positive integer'); // validate # of sdhares
+
+        let currentShares = this.stocks.get(symbol);    // get the current shares for the stock requested
+        let remainingShares = currentShares - shares;
+
+        if (remainingShares > 0) {
+            this.stocks.set(symbol, remainingShares);
+        } else {
+            this.stocks.delete(symbol); // remove all stock if shares are sold
+        }
+    }
+
+    getStockCount() {
+        return this.stocks.size;
     }
 }
 
--- packages/tdd-unittesting/unit.test.js
+++ packages/tdd-unittesting/unit.test.js
@@ -15,5 +15,27 @@ describe('Stock Portfolio', () => {
         expect(portfolio.isEmpty()).toBe(false);    // portfolio should have something in it, thus false
     })
 
-    
+    test('2.3 Make a purchase. Given a symbol and # of shares, (add shares to a symbol).', () => {
+        const portfolio = new StockPortfolio();
+        portfolio.addStock('APPL', 15); // buy a stock using addStock() method.
+        
+        expect(portfolio.isEmpty()).toBe(false);    // portfolio should have a stock in it
+        expect(portfolio.stocks.get('APPL')).toBe(15);  // portfolio should have 15 APPL shares
+    })
+
+    test('2.4 Make a sale. Args: SYMBOL, SHARE -> subtract shares from a symbol', () => {
+        const portfolio = new StockPortfolio();
+        portfolio.addStock('APPL', 15); // first buy stocks from appl
+        portfolio.sellStock('APPL', 10);    //  sell 10 stocks from APPL
+
+        expect(portfolio.stocks.get('APPL')).toBe(5);   // APPL should only have 5 shares left
+    })
+
+    test('2.5 Portfolio should return # of unique stock symbols', () => {
+        const portfolio = new StockPortfolio();
+        portfolio.addStock('APPL', 15); // buy stocks from appl
+        portfolio.addStock('GOOG', 50); // buy stocks from google
+
+        expect(portfolio.getStockCount()).toBe(2);  // two unique stocks should be in the portfolio
+    })
 });
